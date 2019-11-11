1d0334a9679ad841b78ba4be9afd7e1a419b1997
Joe Mainwaring <joe@highground.com>
2019-11-10 18:35:18 -0600

    entrypoint tweak

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-balena login $API_TOKEN && cd $APPLICATION_PATH && balena push $APPLICATION_NAME
+balena login --token $API_TOKEN && cd $APPLICATION_PATH && balena push $APPLICATION_NAME
