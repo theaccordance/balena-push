99e34324f41b7771124c8f96c7f76afb693d4415
Joe Mainwaring <joe@highground.com>
2019-11-15 13:55:10 -0600

    remove debugging code from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/sh -l
-env
 
-echo "Change directory to application path"
-cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH}
-
-echo "debug the path"
-pwd
-ls -la
-
-echo "Login to BalenaCloud"
-balena login --token ${API_TOKEN} && balena push ${APPLICATION_NAME}
+cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} && balena login --token ${API_TOKEN} && balena push ${APPLICATION_NAME}
