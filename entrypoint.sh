f072b18b31452118ce8e5e4985398540e84785fe
Amin Shah Gilani <amin@gilani.me>
2020-06-30 23:18:50 +0500

    Use standalone balena

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} && balena login --token ${API_TOKEN} && balena push ${APPLICATION_NAME}
+cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} && /balena-cli/balena login --token ${API_TOKEN} && /balena-cli/balena push ${APPLICATION_NAME}
